a1a560fe10167d4bac7bc43b603e62b45a71b3de
Luis1080931 <luisu0616@gmail.com>
2023-04-21 10:22:37 -0500

    new

---
+++ js/ficha.js
@@ -0,0 +1,95 @@
+function getficha() {
+
+  let producto = document.getElementById("nom_product").value
+  document.getElementById("resproduct").innerText = producto 
+
+  let linea = document.getElementById("li_product").value
+  document.getElementById("resli").innerText = linea
+
+  let versiones = document.getElementById("ver_product").value
+  document.getElementById("resver").innerText = versiones 
+
+  let version = document.getElementById("act_product").value
+  document.getElementById("resact").innerText = version 
+
+  let modulo = document.getElementById("mod_product").value
+  document.getElementById("resmod").innerText = modulo
+
+  let descripcion = document.getElementById("des_product").value
+  document.getElementById("resdes").innerText = descripcion
+
+  let objetivo = document.getElementById("obj_product").value
+  document.getElementById("resobj").innerText = objetivo
+
+  let arquitectura = document.getElementById("arq_product").value
+  document.getElementById("resarq").innerText = arquitectura
+
+  let hardware = document.getElementById("har_product").value
+  document.getElementById("reshar").innerText = hardware
+
+  let software = document.getElementById("sof_product").value
+  document.getElementById("ressof").innerText = software
+
+  let otros = document.getElementById("otr_product").value
+  document.getElementById("resotr").innerText = otros
+
+  let fisico = document.getElementById("cli_product").value
+  document.getElementById("rescli").innerText = fisico
+
+  let logico = document.getElementById("log_product").value
+  document.getElementById("reslog").innerText = logico
+
+  let aplicativo = document.getElementById("apl_product").value
+  document.getElementById("resapl").innerText = aplicativo
+
+  let funcionales = document.getElementById("fun_product").value
+  document.getElementById("resfun").innerText = funcionales 
+
+  let adicionales = document.getElementById("adi_product").value
+  document.getElementById("resadi").innerText = adicionales 
+
+  let caracteristicas = document.getElementById("car_product").value
+  document.getElementById("rescar").innerText = caracteristicas 
+
+  let versionta = document.getElementById("version").value
+  document.getElementById("respu_ver").innerText = versionta 
+
+  let actualizacion = document.getElementById("actua").value
+  document.getElementById("respuac").innerText = actualizacion 
+
+  let aprovacion = document.getElementById("aprova").value
+  document.getElementById("respuap").innerText = aprovacion 
+
+  let validacion = document.getElementById("vali").value
+  document.getElementById("respuva").innerText = validacion 
+
+  let versi = document.getElementById("versi").value
+  document.getElementById("respues").innerText = versi
+
+  let actuali = document.getElementById("actuali").value
+  document.getElementById("respuesac").innerText = actuali
+
+  let aprovaci = document.getElementById("aprovaci").value
+  document.getElementById("respuesap").innerText = aprovaci
+
+  let validaci = document.getElementById("validaci").value
+  document.getElementById("respuesva").innerText = validaci
+
+  let fecha1 = document.getElementById("fecha1").value
+  document.getElementById("resfe1").innerText = fecha1
+
+  let fecha2 = document.getElementById("fecha2").value
+  document.getElementById("resfe2").innerText = fecha2
+
+  let fecha3 = document.getElementById("fecha3").value
+  document.getElementById("resfe3").innerText = fecha3
+
+  let firma1 = document.getElementById("firma1").value
+  document.getElementById("resfi1").innerText = firma1
+
+  let firma2 = document.getElementById("firma2").value
+  document.getElementById("resfi2").innerText = firma2
+
+  let firma3 = document.getElementById("firma3").value
+  document.getElementById("resfi3").innerText = firma3
+}
